2f14551ebd10b101cc02c3adbca4c8137c5663e8
arifinmn11 <nurarifinmuhammad11111996@gmail.com>
2020-09-03 16:35:30 +0700

    update tugas-9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form Pembelian Buah</h1>
+      <div className="container">
+        <table className="table-form">
+          <tbody>
+            <tr>
+              <td> Nama Pelanggan</td>
+              <td>
+                <input type="text" />
+              </td>
+            </tr>
+            <tr>
+              <td> Daftar Item</td>
+              <td>
+                <div>
+                  <input type="checkbox"></input>
+                  <label>Semangka</label>
+                </div>
+                <div>
+                  <input type="checkbox"></input>
+                  <label>Jeruk</label>
+                </div>
+                <div>
+                  <input type="checkbox"></input>
+                  <label>Nanas</label>
+                </div>
+                <div>
+                  <input type="checkbox"></input>
+                  <label>Salak</label>
+                </div>
+                <div>
+                  <input type="checkbox"></input>
+                  <label>Anggur</label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button> Kirim </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
